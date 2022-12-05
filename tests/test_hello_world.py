12c8b751b9b4847c853847b53749a2da0194bff9
0p553cd3v <0p553cd3v@gmail.com>
2022-12-05 23:05:37 +0100

    [PCL-3] Build config - not working yet

---
+++ tests/test_hello_world.py
@@ -0,0 +1,9 @@
+import unittest
+from base import hello_world 
+
+class TestHelloWorld(unittest.TestCase):
+
+	def test_hello_world(self):
+		self.assertEqual(print_hello_world(), "Hello world")
+
+unittest.main()
