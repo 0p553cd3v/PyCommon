c8aef4d2b83f0ac84fc5b5cb3e1a22bc2cc1e106
0p553cd3v <0p553cd3v@gmail.com>
2022-12-10 23:27:29 +0100

    [PCL-11] Create base github workflows

---
+++ tools/build_and_test.py
@@ -0,0 +1,16 @@
+"""Script to run all tests"""
+
+#Imports
+import os
+
+#Finding build path based on build.py script location
+file_path = os.path.dirname(__file__)
+project_config_path = os.path.abspath(os.path.join(file_path, os.pardir))
+
+#Changing directory to project config path
+os.chdir(project_config_path)
+
+#Run build 
+os.system("python3 build/build.py")
+#Run  tests 
+os.system("python3 tests/tests_run.py")
