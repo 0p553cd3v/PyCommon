b5a368c2ec69be47d3cfb4ccd75aa22817a54856
0p553cd3v <0p553cd3v@gmail.com>
2022-12-18 23:28:04 +0100

    [PCL-30] Refinement of script to display vulture and other in more consistent way

--- tools/run.py
+++ tools/run.py
@@ -9,6 +9,38 @@ import os
 import sys
 import subprocess
 
+#Define functions
+
+def print_line_separator_with_title(title, separator, line_length):
+    '''Function to print separation line with some title inside'''
+    try:
+        if len(separator) != 1:
+            raise Exception("Separator length not equal to 1")
+        if len(title) > line_length:
+            raise Exception("Title length greater than line_length")
+        #Initializing string of separators as a list
+        separator_only_str=[]
+        #Filling empty string with separators only
+        for i in range(int(line_length) - len(title)):
+            separator_only_str.append(separator)
+        #Finding middle position of separator only string
+        mid_pos = len(separator_only_str) // 2
+        #Extending string as a list by title
+        separation_line = separator_only_str[:mid_pos] + [title] + separator_only_str[mid_pos:]
+        #Joining string as a list to create regular string
+        separation_line = ''.join(separation_line)
+        #print
+        print(separation_line)
+    except ValueError as e:
+        print("Wrong function input value: " + str(e))
+        sys.exit(1)
+    except TypeError as e:
+        print("Wrong function input type: " + str(e))
+        sys.exit(1)
+    except Exception as e:
+        print("Exception: " + str(e))
+        sys.exit(1)
+
 #Main function def
 def main():
     file_path = os.path.dirname(__file__)
@@ -20,8 +52,10 @@ def main():
     #Set temporarily PYTHONPATH to src catalogue to fing source code of modules first
     os.environ["PYTHONPATH"] = os.path.join(project_config_path, "src")
 
+    #Run black formater 
+    print_line_separator_with_title(" Black formatter ","-",100)
     try:
-        #Run black formater 
+        
         subprocess.check_call(
             [
                 "tools/checkers/black_run.py",
@@ -29,9 +63,11 @@ def main():
         ) 
     except subprocess.CalledProcessError as e:
         return e.returncode
-
+    
+    #Run vulture checker    
+    print_line_separator_with_title(" Vulture dead code checker ","-",100)
     try:
-        #Run vulture checker 
+
         subprocess.check_call(
             [
                 "tools/checkers/vulture_run.py",
@@ -40,8 +76,10 @@ def main():
     except subprocess.CalledProcessError as e:
         return e.returncode
 
+    #Run lizard CCN analyzer 
+    print_line_separator_with_title(" Lizard cyclomatic complexity analyzer ","-",100)
     try:
-        #Run lizard CCN analyzer 
+        
         subprocess.check_call(
             [
                 "tools/checkers/lizard_run.py",
@@ -50,8 +88,10 @@ def main():
     except subprocess.CalledProcessError as e:
         return e.returncode
 
+    #Run pylint checker
+    print_line_separator_with_title(" PyLint linter checker ","-",100)
     try:
-        #Run pylint checker 
+         
         subprocess.check_call(
             [
                 "tools/checkers/pylint_run.py",
@@ -60,8 +100,9 @@ def main():
     except subprocess.CalledProcessError as e:
         return e.returncode
 
+    #Run unit tests
+    print_line_separator_with_title(" Unit tests ","-",100)
     try:
-        #Run unit tests
         subprocess.check_call(
             [
                 "tests/unit_tests/unit_tests_run.py",
@@ -70,8 +111,9 @@ def main():
     except subprocess.CalledProcessError as e:
         return e.returncode
 
+    #Run integration tests 
+    print_line_separator_with_title(" Integration tests ","-",100)
     try:
-        #Run integration tests 
         subprocess.check_call(
             [
                 "tests/integration_tests/integration_tests_run.py",
