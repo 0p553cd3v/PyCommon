74fc8de3cfc4c9c0a596587273cc8a1b6388c02e
0p553cd3v <0p553cd3v@gmail.com>
2022-12-30 20:37:52 +0100

    [PCL-64] Added some base config for Sphinx - still not working properly

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,15 +18,32 @@ release = "1.0.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+numfig = True
+
+extensions = ['myst_parser']
+
+extensions.append('sphinx.ext.todo')
+extensions.append('sphinx.ext.autodoc')
+extensions.append('sphinx.ext.autosummary')
+extensions.append('sphinx.ext.viewcode')
+extensions.append('sphinx.ext.coverage')
+extensions.append('sphinx.ext.doctest')
+extensions.append('sphinx.ext.graphviz')
+extensions.append('sphinx.ext.ifconfig')
+extensions.append('sphinx.ext.napoleon')
+
+autosummary_generate = True
 
 templates_path = ['_templates']
 exclude_patterns = []
+source_suffix = '.md'
 
-
+pygments_style = 'sphinx'
+todo_include_todos = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'classic'
 html_static_path = ['_static']
+
